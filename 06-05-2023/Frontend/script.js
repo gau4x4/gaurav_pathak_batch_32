5f63c4758b4eb08d5fe3a6d62b24754ccfb12a24
Gaurav Pathak <gauravp361@gmail.com>
2023-06-06 01:55:28 -0500

    [Assignment Issue-21]

--- 06-05-2023/Frontend/script.js
+++ 06-05-2023/Frontend/script.js
@@ -1,20 +1,55 @@
-console.table("helulow");
+// console.table("helulow");
 
-function resolveAfter2Seconds() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("resolved");
-    }, 2000);
-  });
-}
+// function resolveAfter2Seconds() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("resolved");
+//     }, 2000);
+//   });
+// }
 
-async function asyncCall() {
-  console.log("calling");
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-  // Expected output: "resolved"
-}
+// async function asyncCall() {
+//   console.log("calling");
+//   const result = await resolveAfter2Seconds();
+//   console.log(result);
+//   // Expected output: "resolved"
+// }
 
-asyncCall();
+// asyncCall();
 
 // object_var.constructor.prototype == Object.create(name of the object)
+
+const formHandler = function () {
+  const fname = document.querySelector("[name=fname]").value;
+  const lname = document.querySelector("[name=lname]").value;
+  const dob = document.querySelector("[name=dob]").value;
+  const email = document.querySelector("[name=email]").value;
+  const phone = document.querySelector("[name=phone]").value;
+  const college = document.querySelector("[name=college]").value;
+  const company = document.querySelector("[name=company]").value;
+  const obj = {};
+  obj.fName = fname;
+  obj.lName = lname;
+  obj.dob = dob;
+  obj.email = email;
+  obj.phone = phone;
+  obj.college = college;
+  obj.company = company;
+  const ul = document.querySelector(".ul-dataDom");
+  ul.innerHTML = "";
+  for (const [key, value] of Object.entries(obj)) {
+    const li = document.createElement("li");
+    const label = document.createElement("label");
+    const p = document.createElement("p");
+    label.innerText = key;
+    p.innerText = value;
+    li.appendChild(label);
+    li.appendChild(p);
+    ul.appendChild(li);
+  }
+};
+
+const primary_button = document.querySelector(".primary_button");
+primary_button.addEventListener("click", function () {
+  formHandler();
+});
